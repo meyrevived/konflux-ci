6349f89f49358978584aec99ac7c8d0490861c2b
mrath <mrath@redhat.com>
2026-04-28 01:55:03 +0300

    KFLUXINFRA-3633 - only the DR test suite cases are added

--- wait-for-all.sh
+++ wait-for-all.sh
@@ -2,9 +2,9 @@
 
 main() {
     echo "⏳ Waiting for Tekton configuration to be ready..." >&2
-    kubectl wait --for=condition=Ready tektonconfig/config --timeout=120s
+    kubectl wait --for=condition=Ready tektonconfig/config --timeout=360s
     echo "⏳ Waiting for all deployments to be available..." >&2
-    kubectl wait --for=condition=Available deployment --all -A --timeout=240s
+    kubectl wait --for=condition=Available deployment --all -A --timeout=900s
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
